3f05c65a71f208888c1d79291b135bcec2954aad
positiveoutcomes001 <140208800+positiveoutcomes001@users.noreply.github.com>
2025-03-31 22:19:21 +0100

    changes

--- comboGenerator.js
+++ comboGenerator.js
@@ -75,10 +75,10 @@ var FI = [airChair, flagFreeze, hollowBack, invert, pikeFreeze, sideFreeze, oneH
 
 //Move and Freeze Arrays 
 
-var PME = [windmill, swipe, backSpin, tripod, coinDrop, babyFreeze, headstand, headElbowFreeze, lFreeze, shoulderFreeze, turtleFreeze]
-var PMM = [flare, headspin, barrelmill, backSpin, bellymill, tapMill, coinDrop, turtle, swipe ][airBaby, babyFreeze,elbowFreeze,shoulderFreeze, sideFreeze, turtleFreeze]
-var PMH = [halo, ninety, twoThousand, munchmill, ufo, backSpin, bellymill, nutcrackers, turtle, elbowTrack, gorillas, flare, headglide, shoulderSpin, airBaby, airChair, elbowFreeze, flagFreeze, handstand, oneHandFreeze, sideFreeze]
-var PMI = [airFlare, ninety, twoThousand, halo, backSpin, ufo, virginFlare, handcuffs, bellymill, airChairSpin, jackHammer, elbowTrack, elbowSpin, headglide, shoulderSpin, handHop, tombstone, airChair, flagFreeze, hollowBack, invert, pikeFreeze, sideFreeze, oneHandAirBaby]
+var PMFE = [windmill, swipe, backSpin, tripod, coinDrop, babyFreeze, headstand, headElbowFreeze, lFreeze, shoulderFreeze, turtleFreeze]
+var PMFM = [flare, headspin, barrelmill, backSpin, bellymill, tapMill, coinDrop, turtle, swipe ][airBaby, babyFreeze,elbowFreeze,shoulderFreeze, sideFreeze, turtleFreeze]
+var PMFH = [halo, ninety, twoThousand, munchmill, ufo, backSpin, bellymill, nutcrackers, turtle, elbowTrack, gorillas, flare, headglide, shoulderSpin, airBaby, airChair, elbowFreeze, flagFreeze, handstand, oneHandFreeze, sideFreeze]
+var PMFI = [airFlare, ninety, twoThousand, halo, backSpin, ufo, virginFlare, handcuffs, bellymill, airChairSpin, jackHammer, elbowTrack, elbowSpin, headglide, shoulderSpin, handHop, tombstone, airChair, flagFreeze, hollowBack, invert, pikeFreeze, sideFreeze, oneHandAirBaby]
 /*
 
 INSTRUCTIONS 
@@ -296,3 +296,167 @@ function FE6(){
 } 
 
 //Medium Freeze Functions 
+function FM3(){
+    var FM = [airBaby, babyFreeze,elbowFreeze,shoulderFreeze, sideFreeze, turtleFreeze]
+    FM.sort(() => Math.random() - 0.5);
+    var freezes = FM.splice (0, 3);     
+    sessionStorage.setItem("freezes", freezes);
+    
+ document.getElementById("freezes").innerHTML = freezes
+
+} 
+
+function FM4(){
+    var FM = [airBaby, babyFreeze,elbowFreeze,shoulderFreeze, sideFreeze, turtleFreeze]
+    FM.sort(() => Math.random() - 0.5);
+    var freezes = FM.splice (0, 4);     
+    sessionStorage.setItem("freezes", freezes);
+    
+ document.getElementById("freezes").innerHTML = freezes
+
+} 
+
+function FM5(){
+    var FM = [airBaby, babyFreeze,elbowFreeze,shoulderFreeze, sideFreeze, turtleFreeze]
+    FM.sort(() => Math.random() - 0.5);
+    var freezes = FM.splice (0, 5);     
+    sessionStorage.setItem("freezes", freezes);
+    
+ document.getElementById("freezes").innerHTML = freezes
+
+} 
+
+function FM6(){
+    var FM = [airBaby, babyFreeze,elbowFreeze,shoulderFreeze, sideFreeze, turtleFreeze]
+    FM.sort(() => Math.random() - 0.5);
+    var freezes = FM.splice (0, 6);     
+    sessionStorage.setItem("freezes", freezes);
+    
+ document.getElementById("freezes").innerHTML = freezes
+
+} 
+//Hard Freeze Functions 
+function FH3(){
+    var FH = [airBaby, airChair, elbowFreeze, flagFreeze, handstand, oneHandFreeze, sideFreeze]
+    FH.sort(() => Math.random() - 0.5);
+    var freezes = FH.splice (0, 3);     
+    sessionStorage.setItem("freezes", freezes);
+    
+ document.getElementById("freezes").innerHTML = freezes
+
+} 
+
+function FH4(){
+    var FH = [airBaby, airChair, elbowFreeze, flagFreeze, handstand, oneHandFreeze, sideFreeze]
+    FH.sort(() => Math.random() - 0.5);
+    var freezes = FH.splice (0, 4);     
+    sessionStorage.setItem("freezes", freezes);
+    
+ document.getElementById("freezes").innerHTML = freezes
+
+} 
+
+function FH5(){
+    var FH = [airBaby, airChair, elbowFreeze, flagFreeze, handstand, oneHandFreeze, sideFreeze]
+    FH.sort(() => Math.random() - 0.5);
+    var freezes = FH.splice (0, 5);     
+    sessionStorage.setItem("freezes", freezes);
+    
+ document.getElementById("freezes").innerHTML = freezes
+
+} 
+
+function FH6(){
+    var FH = [airBaby, airChair, elbowFreeze, flagFreeze, handstand, oneHandFreeze, sideFreeze]
+    FH.sort(() => Math.random() - 0.5);
+    var freezes = FH.splice (0, 6);     
+    sessionStorage.setItem("freezes", freezes);
+    
+ document.getElementById("freezes").innerHTML = freezes
+
+} 
+//Insanity Freeze Functions 
+function FI3(){
+    var FI = [airChair, flagFreeze, hollowBack, invert, pikeFreeze, sideFreeze, oneHandAirBaby]
+    FI.sort(() => Math.random() - 0.5);
+    var freezes = FI.splice (0, 3);     
+    sessionStorage.setItem("freezes", freezes);
+    
+ document.getElementById("freezes").innerHTML = freezes
+
+} 
+
+function FI4(){
+    var FI = [airChair, flagFreeze, hollowBack, invert, pikeFreeze, sideFreeze, oneHandAirBaby]
+    FI.sort(() => Math.random() - 0.5);
+    var freezes = FI.splice (0, 4);     
+    sessionStorage.setItem("freezes", freezes);
+    
+ document.getElementById("freezes").innerHTML = freezes
+
+} 
+
+function FI5(){
+    var FI = [airChair, flagFreeze, hollowBack, invert, pikeFreeze, sideFreeze, oneHandAirBaby]
+    FI.sort(() => Math.random() - 0.5);
+    var freezes = FI.splice (0, 5);     
+    sessionStorage.setItem("freezes", freezes);
+    
+ document.getElementById("freezes").innerHTML = freezes
+
+} 
+
+function FI6(){
+    var FI = [airChair, flagFreeze, hollowBack, invert, pikeFreeze, sideFreeze, oneHandAirBaby]
+    FI.sort(() => Math.random() - 0.5);
+    var freezes = FI.splice (0, 6);     
+    sessionStorage.setItem("freezes", freezes);
+    
+ document.getElementById("freezes").innerHTML = freezes
+
+} 
+
+//Move and Freeze Functions 
+//Easy Move and Freeze Functions 
+
+function PMFE3(){
+    var PMFE = [windmill, swipe, backSpin, tripod, coinDrop, babyFreeze, headstand, headElbowFreeze, lFreeze, shoulderFreeze, turtleFreeze]
+    PMFE.sort(() => Math.random() - 0.5);
+    var movesFreezes = PMFE.splice (0, 3);     
+    sessionStorage.setItem("movesFreezes", movesFreezes);
+    
+ document.getElementById("movesFreezes").innerHTML = movesFreezes
+
+} 
+
+function PMFE4(){
+    var PMFE = [windmill, swipe, backSpin, tripod, coinDrop, babyFreeze, headstand, headElbowFreeze, lFreeze, shoulderFreeze, turtleFreeze]
+    PMFE.sort(() => Math.random() - 0.5);
+    var movesFreezes = PMFE.splice (0, 4);     
+    sessionStorage.setItem("movesFreezes", movesFreezes);
+    
+ document.getElementById("movesFreezes").innerHTML = movesFreezes
+
+} 
+
+function PMFE5(){
+    var PMFE = [windmill, swipe, backSpin, tripod, coinDrop, babyFreeze, headstand, headElbowFreeze, lFreeze, shoulderFreeze, turtleFreeze]
+    PMFE.sort(() => Math.random() - 0.5);
+    var movesFreezes = PMFE.splice (0, 5);     
+    sessionStorage.setItem("movesFreezes", movesFreezes);
+    
+ document.getElementById("movesFreezes").innerHTML = movesFreezes
+
+} 
+
+function PMFE6(){
+    var PMFE = [windmill, swipe, backSpin, tripod, coinDrop, babyFreeze, headstand, headElbowFreeze, lFreeze, shoulderFreeze, turtleFreeze]
+    PMFE.sort(() => Math.random() - 0.5);
+    var movesFreezes = PMFE.splice (0, 6);     
+    sessionStorage.setItem("movesFreezes", movesFreezes);
+    
+ document.getElementById("movesFreezes").innerHTML = movesFreezes
+
+} 
+
+//
